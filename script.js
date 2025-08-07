bdf10c1db42c1ce3f6e05923e788eca2e3477bd1
henrique guilherme <121883318+gxhenrique@users.noreply.github.com>
2025-08-07 17:01:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+      breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    // when window width is >= 480px
+    728: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    // when window width is >= 640px
+    940: {
+      slidesPerView: 3,
+      spaceBetween: 10
+    }
+  }
+});
+
